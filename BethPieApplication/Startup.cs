8c99c3714e4c7520f1cb22c106be29a1402089a8
Luis Chaparro <luischaparro57@gmail.com>
2022-05-01 19:43:33 -0500

    Correccion de formato Strtup

--- BethPieApplication/Startup.cs
+++ BethPieApplication/Startup.cs
@@ -16,10 +16,9 @@ namespace BethPieApplication
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllersWithViews();
          }
-
+        
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
