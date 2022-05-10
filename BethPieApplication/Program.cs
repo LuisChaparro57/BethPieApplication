c07de691d53622b01cabc92497bde9e431d917ab
Luis Chaparro <luischaparro57@gmail.com>
2022-05-10 08:47:39 -0500

    Documentación y adiciones en el código

--- BethPieApplication/Program.cs
+++ BethPieApplication/Program.cs
@@ -13,9 +13,14 @@ namespace BethPieApplication
     {
         public static void Main(string[] args)
         {
+            // llama al método CreateHostBuilder
             CreateHostBuilder(args).Build().Run();
         }
 
+        // este método creará un host que ejecutara la aplicación
+        // primero llama Host.CreateDefaultBuilder
+        // configura un generador de host con muchos valores predeterminados
+        // (ver el contenido de cada linea del método(-- I primera linea y remarks)
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
